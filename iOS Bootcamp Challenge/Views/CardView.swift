1730b8c090ad66738d3b36e0fc617a63fa696210
Iktan <trejographics@gmail.com>
2021-10-02 21:11:49 -0500

    Exercise V
    V. Display pokemon info (eg. types, abilities)

--- iOS Bootcamp Challenge/Views/CardView.swift
+++ iOS Bootcamp Challenge/Views/CardView.swift
@@ -20,6 +20,24 @@ final class CardView: UIView {
         return label
     }()
 
+    lazy private var typesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy private var abilitiesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     required init(card: Card) {
         self.card = card
         super.init(frame: .zero)
@@ -27,7 +45,7 @@ final class CardView: UIView {
         setupUI()
     }
 
-    required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) { 
         super.init(coder: coder)
         setup()
         setupUI()
@@ -36,20 +54,41 @@ final class CardView: UIView {
     private func setup() {
         guard let card = card else { return }
 
-        card.items.forEach { _ in }
+        var types = ""
+        var abilities = ""
+        
+        card.items.forEach { item in
+            
+            if item.title == "Types" {
+                types = "🌎 " + item.title + "\n" + item.description.capitalized
+            } else if item.title == "Abilities" {
+                abilities = "👌 " + item.title + "\n" + item.description.capitalized
+            }
+        }
 
         titleLabel.text = card.title
+        typesLabel.text = types
+        abilitiesLabel.text = abilities
         backgroundColor = .white
         layer.cornerRadius = 20
     }
 
     private func setupUI() {
         addSubview(titleLabel)
+        addSubview(typesLabel)
+        addSubview(abilitiesLabel)
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin * 2).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
         titleLabel.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 0.70).isActive = true
 
-        // TODO: Display pokemon info (eg. types, abilities)
+        typesLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: margin).isActive = true
+        typesLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
+        typesLabel.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 0.70).isActive = true
+        
+        abilitiesLabel.topAnchor.constraint(equalTo: typesLabel.bottomAnchor, constant: margin).isActive = true
+        abilitiesLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
+        abilitiesLabel.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 0.70).isActive = true
+        
     }
 
 }
